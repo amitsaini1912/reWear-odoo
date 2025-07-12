ed3edb3e36f17195dfa701197035185f81edf7a4
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-07-12 10:35:21 +0530

    Sign Up page added and routes and views folder also created

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 let port = 8000;
 const mongoose = require("mongoose");
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const path = require('path');
+require('dotenv').config();
+
+
 
 main()
   .then( () => {
@@ -13,14 +19,33 @@ main()
   });
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/reWaer');
+  await mongoose.connect("mongodb://127.0.0.1:27017/reWaer");
 };
 
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+// Session
+app.use(session({
+    secret: 'rewear-secret',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl:  "mongodb://127.0.0.1:27017/reWaer"})
+}));
+
+
+// Routes
+app.use('/', require('./routes/auth'));
 
 //Index Route
 app.get("/", (req, res) => {
   res.send("Hi, I am root" );
 });
+
 
 
 app.listen(port, () => {
---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+// Signup
+router.get('/signup', (req, res) => res.render('pages/signup'));
+router.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const hash = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hash });
+        req.session.user = user;
+        res.redirect('/dashboard');
+    } catch (err) {
+        res.send('Signup Error: ' + err.message);
+    }
+});
+
+
+module.exports = router;
